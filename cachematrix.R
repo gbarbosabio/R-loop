45f4bad04c8fe0ff4038fafd7695219c0e2345c3
gbarbosabio <gbarbosa.bio@gmail.com>
2016-01-24 23:31:53 -0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## As far as I understand, the function gets matrix x and inverts it with solve
+## function. Than if a matrix x gets the same inverted matrix it returns the
+## message "getting cached data" and the inverted matrix. If not it inversts the 
+## new matrix
 
-## Write a short comment describing this function
+## It gets the inverted matrix from a certain matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## If checks if the inverted solution already exists, and if it doesnt it gets the
+## inverted matrix
 cacheSolve <- function(x, ...) {
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
         ## Return a matrix that is the inverse of 'x'
 }
